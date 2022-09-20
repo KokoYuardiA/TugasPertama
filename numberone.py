b3cd02f559863d2287d76cdc8b7d95af8ff1d842
KokoYuardiA <11201048@student.itk.ac.id>
2022-09-21 00:14:07 +0800

    update

--- numberone.py
+++ numberone.py
@@ -1,8 +1,8 @@
-def sum_squares(*numbers) :
-    arr = [*numbers]
-    temp = 0
-    for num in arr:
-        temp += num**2
-    return temp
+def sum_squares(*angka) :
+    listNumber = [*angka]
+    ss = 0
+    for num in listNumber:
+        ss += num**2
+    return ss
 
 print(sum_squares(1, 2, 3))
